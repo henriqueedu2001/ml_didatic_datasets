2a7f9fd4be298e67a7da55e5e92a4ee1ff2036ae
Henrique Eduardo <henrique_eduardo_souza@hotmail.com>
2023-10-20 13:27:11 -0300

    ✨ New Feature: Binomial dist

--- src/generator.py
+++ src/generator.py
@@ -10,7 +10,7 @@ class Generator:
             max_value (float): valor máximo da distribuição
 
         Returns:
-            _type_: _description_
+            np.array: dataaset com valores distribuídos uniformemente
         """
         # geranção de n=dataset_size valores no intervalo [0,1)
         dataset = np.random.random(dataset_size)
@@ -33,3 +33,17 @@ class Generator:
             np.array: dataset com valores distribuídos normalmente
         """
         return np.random.normal(mean, std_dev, dataset_size)
+    
+    
+    def generate_binomial_dist(dataset_size: int, sucess_prob: float, n: int) -> np.array:
+        """Gera uma distribuição binomial 
+
+        Args:
+            dataset_size (int): quantidade de pontos do dataset
+            sucess_prob (float): probabilidade de sucesso
+            n (int): parâmetro n da distribuição
+
+        Returns:
+            np.array: dataset com os valores distribuídos binomialmente
+        """
+        return np.random.binomial(n, sucess_prob, dataset_size)
